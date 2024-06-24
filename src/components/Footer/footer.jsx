2e112c7f00c07871f489cad366761a94f43cd741
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-06-24 12:41:46 +0200

    Rozdělení stránky do samostaných komponent

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css"
+
+export const Footer = () => (
+    <footer>
+        <div className="container">
+        <div className="footer__content">
+            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+        </div>
+        </div>
+    </footer>
+)
